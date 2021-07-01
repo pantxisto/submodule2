7124d2dcfbf26a596bf5a6956bba64736d322c60
DeveloperBilbao91 <jonandonicastelo@outlook.com>
2021-07-01 17:14:47 +0200

    First commit submodule 2

---
+++ src/test/java/com/example/Submodule2/Submodule2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Submodule2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Submodule2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
